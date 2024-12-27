ffca602f92f3bbcbd51c26cbda55de6c3f727111
Stefania Peter <1927103+cat-a-flame@users.noreply.github.com>
2024-12-27 17:52:18 +0100

    New design, yay!

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/js');
+  eleventyConfig.addPassthroughCopy('src/fonts');
 
   // Filters
   Object.keys(filters).forEach(filterName => {
