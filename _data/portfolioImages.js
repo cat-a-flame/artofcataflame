bf5de563f4d33b04682ecade6b3157af620be03e
Stefánia Péter <1927103+cat-a-flame@users.noreply.github.com>
2025-06-07 16:39:07 +0200

    Sort portfolio images by modified date

--- _data/portfolioImages.js
+++ _data/portfolioImages.js
@@ -5,9 +5,15 @@ module.exports = () => {
   const dir = path.join(__dirname, '..', 'src', 'images', 'illustrations');
   return fs.readdirSync(dir)
     .filter(file => /\.(jpe?g|png|gif)$/i.test(file))
-    .map(file => ({
-      src: `/src/images/illustrations/${file}`,
-      filePath: path.join(dir, file),
-      name: path.parse(file).name
-    }));
+    .map(file => {
+      const filePath = path.join(dir, file);
+      return {
+        src: `/src/images/illustrations/${file}`,
+        filePath,
+        name: path.parse(file).name,
+        mtime: fs.statSync(filePath).mtime
+      };
+    })
+    .sort((a, b) => b.mtime - a.mtime)
+    .map(({ mtime, ...img }) => img);
 };
