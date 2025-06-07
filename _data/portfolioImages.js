03e4b94a7f693dd713c355bedf76f7e37f66d163
Stefánia Péter <1927103+cat-a-flame@users.noreply.github.com>
2025-06-07 16:00:20 +0200

    Automate portfolio images

---
+++ _data/portfolioImages.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = () => {
+  const dir = path.join(__dirname, '..', 'src', 'images', 'illustrations');
+  return fs.readdirSync(dir)
+    .filter(file => /\.(jpe?g|png|gif)$/i.test(file))
+    .map(file => ({
+      src: `/src/images/illustrations/${file}`,
+      name: path.parse(file).name
+    }));
+};
